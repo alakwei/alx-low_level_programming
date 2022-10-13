b0dad8735d54300610323a5e63d4f081ceb8313d
alakwei <alakwei@gmail.com>
2022-10-13 11:48:24 -0700

    Task 4, Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 
