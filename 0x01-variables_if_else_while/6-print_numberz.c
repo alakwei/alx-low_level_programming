da49f1e5083c742a44df074ff39ed71d2cc42d0c
alakwei <alakwei@gmail.com>
2022-10-15 18:17:27 -0700

    Task 6, Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - single digits b 10
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
