a8505fcbde48ceafc32da73ce1d2070ca049c3ce
alakwei <alakwei@gmail.com>
2022-10-15 18:25:09 -0700

    Task 7, Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase reverse alphabets
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char r;
+
+	for (r = 'z'; r >= 'a'; r--)
+		putchar(r);
+		putchar('\n');
+	return (0);
+}
+
+
