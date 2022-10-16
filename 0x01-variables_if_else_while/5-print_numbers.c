b3a34f34f06fd3c09eca96ae48741042eecb0449
alakwei <alakwei@gmail.com>
2022-10-15 18:02:27 -0700

    Task 5, Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - single digit numbers
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+		putchar('\n')
+	return (0);
+}
