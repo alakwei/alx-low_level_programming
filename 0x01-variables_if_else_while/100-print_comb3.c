76e4120882782d832517fe33029b64ae073faaea
alakwei <alakwei@gmail.com>
2022-10-16 13:51:11 -0700

    Task 10, nventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - possible different
+ * different combination
+ * of two digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x, y = '0', z;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+	for (z = y; z <= '9'; z++)
+	{
+	if (x != z)
+	{
+		putchar(x);
+		putchar(z);
+	}
+	if (x == z)
+	{
+	continue;
+	}
+	if (x == '8' && z == '9')
+	{
+	break;
+	}
+	else
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	k++;
+	}
+		putchar('\n');
+	return (0);
+}
