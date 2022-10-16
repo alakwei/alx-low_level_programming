640eac0e53a3948ae8eaaf4561ed9068bcc919d5
alakwei <alakwei@gmail.com>
2022-10-15 18:47:32 -0700

    Task 8, Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - numbers and alphabets b 16
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+	char z;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (z = 'a'; z <= 'f'; z++)
+	{
+		putchar(z);
+	}
+		putchar('\n')
+	return (0);
+}
