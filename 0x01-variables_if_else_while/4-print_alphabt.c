0bc9b7cf44513d95921019b79d59c07a0daa04bc
alakwei <alakwei@gmail.com>
2022-10-15 17:44:11 -0700

    Task 4, When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets without q and e
+ *
+ * Return: Always (Successful)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x == 'e' || x == 'q'
+		continue;
+		putchar('\n');
+	return (0);
+}
