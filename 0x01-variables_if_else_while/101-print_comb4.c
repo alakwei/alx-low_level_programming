fc00af113a3e81a6b1ca094a636a93d3fabca5b4
alakwei <alakwei@gmail.com>
2022-10-16 15:00:27 -0700

    Task 11, The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - all possible combination
+ * of different three digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+	for (b = '0'; b <= '9'; b++)
+	{
+	for (c = '0'; c <= '9'; c++)
+	{
+	if (a < b && b < c)
+	{
+		putchar(a);
+		putchar(b);
+		putchar(c);
+	if (a == '7' && b == '8' && c == '9')
+	{
+	break;
+	}
+	else
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	}
+	}
+		putchar('\n');
+	return (0);
+}
+
+
+
