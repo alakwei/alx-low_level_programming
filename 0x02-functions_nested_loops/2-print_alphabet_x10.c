2d4e8dc6f58b0897ab4b06d79999d63ff67c8b85
alakwei <alakwei@gmail.com>
2022-10-19 09:34:00 -0700

    Task 2, 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int t;
+	char az;
+
+	for (t = 0; t <= 9; t++)
+	{
+		for (az = 'a'; az <= 'z'; az++)
+			_putchar(az);
+		_putchar('\n');
+	}
+}
