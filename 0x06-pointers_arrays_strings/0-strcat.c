624279662dc3c5ef210e28e8c28c96bc05724ca1
alakwei <alakwei@gmail.com>
2022-10-26 18:59:48 -0700

    Task 0, strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @src: string 1
+ * @dest: string 2
+ * Return: a poniter
+ */
+
+har *_strcat(char *dest, char *src)
+{
+	int c;
+	int d;
+
+	c = 0;
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+	d = 0;
+	while (src[d] != '\0')
+	{
+		dest[c] = src[d];
+		c++;
+		d++;
+	}
+	dest[c] = '\0';
+	return (dest);
+}
+
